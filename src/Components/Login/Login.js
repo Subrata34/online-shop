4134850b5d051858c02505d993a7da8b7ce0c655
Subrata34 <subratamodak3491@gmail.com>
2022-06-12 13:44:00 +0600

    files

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,21 +1,35 @@
-import { TextField } from '@mui/material'
-import React from 'react'
+import { Button, TextField } from "@mui/material";
+import React from "react";
 
 const Login = () => {
   return (
     <div>
-        <h1>please login </h1>
-        <form action="">
-        <TextField 
-        id="standard-basic" 
-        label="Name" 
-        variant="standard"
-        type="name" 
-      />
-
-        </form>
+      <h1>please login </h1>
+      <form action="">
+        <TextField
+          id="standard-basic"
+          label="Name"
+          variant="standard"
+          type="name"
+        />
+        <TextField
+          id="standard-basic"
+          label="Email"
+          variant="standard"
+          type="email"
+        />
+        <TextField
+          id="standard-basic"
+          label="Password"
+          variant="standard"
+          type="password"
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      <br />
+      <Button variant="contained">Google Sgin In</Button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

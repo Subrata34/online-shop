b3bbc8f949c26d1fd5ecddc677c91595aeead602
Subrata34 <subratamodak3491@gmail.com>
2022-06-14 11:20:51 +0600

    files

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,4 +1,5 @@
 
+import { Input, TextField } from "@mui/material";
 import React from "react";
 
 const Registration = () => {
@@ -7,7 +8,27 @@ const Registration = () => {
       <h1>Registration Please</h1>
       <div>
         <form action="">
-         
+        
+        <TextField
+          id="standard-basic"
+          label="Name"
+          variant="standard"
+          type="name"
+        />
+        <TextField
+          id="standard-basic"
+          label="Email"
+          variant="standard"
+          type="email"
+        />
+        <TextField
+          id="standard-basic"
+          label="Password"
+          variant="standard"
+          type="password"
+        />
+        <Input type="submit" value="Submit" />
+      
         </form>
       </div>
     </div>

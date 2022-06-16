bdf648dca2036ed8020536cfc71c404ae8fb98c1
Subrata34 <subratamodak3491@gmail.com>
2022-06-16 22:24:24 +0600

    files

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div> 
       <div>
       <h1>About</h1>
-      <p>This is online shop .Where we can online shopping </p>
+      <p>This is online shop .Where we can online shopping.This website is very small and nice site for online shopping in  digital platfrom  </p>
       </div>
 
     </div>
